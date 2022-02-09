9e2900871b767ec3d77d7cb7a2da65b333103032
ichung14 <34329462+IChung14@users.noreply.github.com>
2022-02-08 20:24:09 -0500

    model blank files added

---
+++ NoteApplication/app/src/main/java/com/noteapplication/cs398/model/ClassModel.kt
@@ -0,0 +1,7 @@
+package com.noteapplication.cs398.model
+
+// what do we need for classes?
+class ClassModel(val title:String){
+
+
+}
---
+++ NoteApplication/app/src/main/java/com/noteapplication/cs398/model/NoteModel.kt
@@ -0,0 +1,8 @@
+package com.noteapplication.cs398.model
+
+import java.sql.Date
+
+class NoteModel(val title: String, val body: String, val createAt: Date, val editedAt: Date){
+
+
+}
