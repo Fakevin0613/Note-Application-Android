97fc4a17f592885901aa55f573eb4aea2877ae40
[Kevin Ke] <[q4ke@uwaterloo.ca]>
2022-02-11 11:03:48 -0500

    source code added

---
+++ Note-Application-master/app/src/main/java/com/gtappdevelopers/noteapplication/NoteRepository.kt
@@ -0,0 +1,22 @@
+package com.gtappdevelopers.noteapplication
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import java.util.concurrent.Flow
+
+class NoteRepository(private val notesDao: NotesDao) {
+
+
+    val allNotes: LiveData<List<Note>> = notesDao.getAllNotes()
+
+    suspend fun insert(note: Note) {
+        notesDao.insert(note)
+    }
+    suspend fun delete(note: Note){
+        notesDao.delete(note)
+    }
+
+    suspend fun update(note: Note){
+        notesDao.update(note)
+    }
+}
