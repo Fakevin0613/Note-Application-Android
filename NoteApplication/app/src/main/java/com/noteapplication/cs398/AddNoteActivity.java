8ccc792c9773c50ca785fb0ea99f741996fc4b56
[Kevin Ke] <[q4ke@uwaterloo.ca]>
2022-02-13 23:37:11 -0500

    kevin02/13

---
+++ NoteApplication/app/src/main/java/com/noteapplication/cs398/AddNoteActivity.java
@@ -0,0 +1,6 @@
+package com.noteapplication.cs398;
+
+import android.app.Activity;
+
+public class AddNoteActivity extends Activity {
+}
--- NoteApplication/app/src/main/java/com/noteapplication/cs398/MainActivity.kt
+++ NoteApplication/app/src/main/java/com/noteapplication/cs398/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.noteapplication.cs398
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val addButton: MaterialButton = findViewById(R.id.notefolderadd);
+
+        addButton.setOnClickListener{
+            val intent = Intent(this@MainActivity, AddNoteActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }
+
+
+
+
     }
 }
