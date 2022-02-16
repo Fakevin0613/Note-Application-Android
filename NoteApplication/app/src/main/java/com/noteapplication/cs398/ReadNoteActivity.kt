d2894f81bd50d259e6f12ae357045a8bfb20f5f9
ichung14 <34329462+IChung14@users.noreply.github.com>
2022-02-16 11:20:02 -0500

    readnoteactivity.java -> .kt

--- NoteApplication/app/src/main/java/com/noteapplication/cs398/ReadNoteActivity.kt
+++ NoteApplication/app/src/main/java/com/noteapplication/cs398/ReadNoteActivity.kt
@@ -2,17 +2,16 @@ package com.noteapplication.cs398
 
 import android.app.Activity
 import android.os.Bundle
-import com.noteapplication.cs398.databinding.AddNoteBinding
 import com.noteapplication.cs398.databinding.ReadNoteBinding
 
 class ReadNoteActivity :Activity(){
-private lateinit var binding:ReadNoteBinding
+    private lateinit var binding:ReadNoteBinding
 
-        override fun onCreate(savedInstanceState:Bundle?){
+    override fun onCreate(savedInstanceState:Bundle?){
         super.onCreate(savedInstanceState)
 
         binding=ReadNoteBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-        }
-        }
+    }
+}
