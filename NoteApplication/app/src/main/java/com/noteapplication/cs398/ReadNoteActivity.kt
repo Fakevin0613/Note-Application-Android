164f8e5c77181732574a7cf75e407171f39d117a
ichung14 <34329462+IChung14@users.noreply.github.com>
2022-02-16 16:20:57 -0500

    navigation from read note to edit note

--- NoteApplication/app/src/main/java/com/noteapplication/cs398/ReadNoteActivity.kt
+++ NoteApplication/app/src/main/java/com/noteapplication/cs398/ReadNoteActivity.kt
@@ -1,20 +1,45 @@
 package com.noteapplication.cs398
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.noteapplication.cs398.databinding.ReadNoteBinding
 
-class ReadNoteActivity :Activity(){
+class ReadNoteActivity : AppCompatActivity(){
+
+    val REQUEST_EDIT = 1
+
     private lateinit var binding:ReadNoteBinding
+
+    private lateinit var noteItem: LiveData<Note?>
 
     override fun onCreate(savedInstanceState:Bundle?){
         super.onCreate(savedInstanceState)
         binding = ReadNoteBinding.inflate(layoutInflater)
 
-        val noteItem = intent.getSerializableExtra("noteItem") as Note
-        binding.noteTitle.setText(noteItem.noteTitle)
-        binding.noteContent.setText(noteItem.noteContent)
+        noteItem = MutableLiveData(intent.getSerializableExtra("noteItem") as Note?)
+
+        noteItem.observe(this){
+            it?.let {
+                binding.noteTitle.text = it.noteTitle
+                binding.noteContent.text = it.noteContent
+            }
+        }
+
+        binding.editButton.setOnClickListener {
+            // make AddNoteActivity catch for the extra and use it as its base Note
+            val intent = Intent(this@ReadNoteActivity, AddNoteActivity::class.java)
+            intent.putExtra("noteItem", noteItem.value)
+            startActivityForResult(intent,REQUEST_EDIT)
+        }
 
         setContentView(binding.root)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
