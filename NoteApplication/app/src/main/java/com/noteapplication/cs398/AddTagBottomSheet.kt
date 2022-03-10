e5d4ce94f243eb6b78439961869b83734acf6f70
Shonn Li <s854li@uwaterloo.ca>
2022-03-09 20:08:12 -0500

    tag feature 2

--- NoteApplication/app/src/main/java/com/noteapplication/cs398/AddTagBottomSheet.kt
+++ NoteApplication/app/src/main/java/com/noteapplication/cs398/AddTagBottomSheet.kt
@@ -6,22 +6,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.noteapplication.cs398.databinding.AddCourseBinding
+import com.noteapplication.cs398.databinding.AddTagBinding
 
 class AddTagBottomSheet(private var viewModel: TagViewModel) : BottomSheetDialogFragment() {
 
-    private lateinit var binding: AddCourseBinding
+    private lateinit var binding: AddTagBinding
     private lateinit var saveBtn: ImageButton
+    private lateinit var tagList: RecyclerView
+    private lateinit var adapter: TagListAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        binding = AddCourseBinding.inflate(inflater, container, false)
-        saveBtn = binding.addNewCourse
+        binding = AddTagBinding.inflate(inflater, container, false)
+        saveBtn = binding.addNewTag
+        tagList = binding.tagList
+//        tagList.layoutManager = LinearLayoutManager(this)
+//        adapter = TagListAdapter(viewModel, this)
+//        adapter.setClickListener(this)
+//        tagList.adapter = adapter
         saveBtn.setOnClickListener() {
             print("clicked")
-            viewModel.insertTag(Tag(binding.courseInput.text.toString()))
+            viewModel.insertTag(Tag(binding.tagInput.text.toString()))
         }
         return binding.root
     }
