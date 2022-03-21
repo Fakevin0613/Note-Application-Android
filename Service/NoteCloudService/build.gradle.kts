00f2601a1e618d3e92d9f8e8f13ceeb9dbc53a6c
ichung14 <34329462+IChung14@users.noreply.github.com>
2022-03-21 10:52:53 -0400

    service implementations

--- Service/NoteCloudService/build.gradle.kts
+++ Service/NoteCloudService/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
-	kotlin("plugin.jpa") version "1.6.10"
 }
 
 group = "com.cs398"
@@ -17,12 +16,12 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- Service/NoteCloudService/src/main/kotlin/com/cs398/NoteCloudService/NoteCloudServiceApplication.kt
+++ Service/NoteCloudService/src/main/kotlin/com/cs398/NoteCloudService/NoteCloudServiceApplication.kt
@@ -1,11 +1,84 @@
 package com.cs398.NoteCloudService
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.*
 
-@SpringBootApplication
+@SpringBootApplication()
 class NoteCloudServiceApplication
 
 fun main(args: Array<String>) {
 	runApplication<NoteCloudServiceApplication>(*args)
+}
+
+@RestController
+class NoteResource(val service: NoteService){
+
+	@GetMapping
+	fun index(): List <Note> = service.findNotes()
+
+	@PostMapping
+	fun post(@RequestBody note: Note){
+		service.post(note)
+	}
+}
+
+@Service
+class NoteService(val dao: NoteRepository){
+	fun findNotes() = dao.findNotes()
+	fun post(note: Note){
+		dao.save(note)
+	}
+}
+
+
+@Table("Note")
+data class Note(
+	val id: Long?,
+	val title: String,
+	val content: String,
+	val notify: Boolean,
+	val
+
+)
+
+//`title` TEXT NOT NULL,
+//`content` TEXT NOT NULL,
+//`notify` INTEGER NOT NULL,
+//`folderId` INTEGER,
+//`createdTime` DATE NOT NULL,
+//`updatedTime` DATE NOT NULL,
+//`id` INTEGER PRIMARY KEY NOT NULL,
+//FOREIGN KEY(`folderId`) REFERENCES `Folder`(`id`) ON UPDATE NO ACTION ON DELETE SET NULL
+
+//`Folder`
+//`name` TEXT NOT NULL,
+//`parent` INTEGER,
+//`id` INTEGER PRIMARY KEY NOT NULL,
+//`createdTime` DATE NOT NULL,
+//`updatedTime` DATE NOT NULL,
+//FOREIGN KEY(`parent`) REFERENCES `Folder`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE
+
+//tag
+//`name` TEXT NOT NULL,
+//`createdTime` DATE NOT NULL,
+//`updatedTime` DATE NOT NULL,
+//`id` INTEGER PRIMARY KEY NOT NULL
+
+
+//CREATE TABLE IF NOT EXISTS `TagNoteCrossRef`
+//(
+//`tagId` INTEGER NOT NULL,
+//`noteId` INTEGER NOT NULL,
+//`createdTime` DATE NOT NULL,
+//`updatedTime` DATE NOT NULL,
+
+interface NoteRepository: CrudRepository<Note, String>{
+	@Query("select id, title from Note")
+	fun findNotes(): List<Note>
 }
